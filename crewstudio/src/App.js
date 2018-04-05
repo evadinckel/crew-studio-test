0acaa46c988f391ac530ec90560ee0d055d4de66
Eva.Dinckel <evadinckel@gmail.com>
2018-04-05 16:19:18 +0100

    create react app set up, readme in progress

---
+++ crewstudio/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          Get inspired by our amazing picture gallery!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
